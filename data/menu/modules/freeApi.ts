6706a450b8be5eeffdc43a5858f0debf53fc48b4
hb0730 <1278032416@qq.com>
2024-12-09 15:28:59 +0800

    fix: 移除【教书先生API】

--- data/menu/modules/freeApi.ts
+++ data/menu/modules/freeApi.ts
@@ -11,13 +11,6 @@ export default {
       tags: ['api', '接口', '免费'],
     },
     {
-      title: '教书先生API',
-      url: 'https://api.oioweb.cn/',
-      logo: 'https://api.oioweb.cn/favicon.ico',
-      description: '教书先生API 是 教书先生 支持并维护的 API 接口项目，致力于为用户提供稳定、快速的免费 API 接口服务平台。',
-      tags: ['api', '接口', '免费'],
-    },
-    {
       title: '宋子宪API',
       url: 'https://api.songzixian.com/',
       description: 'API-领先的高性能免费API数据服务平台。我们专注于提供稳定、高速的数据接口服务，助您轻松实现各类应用。 本api服务系统是自研系统，采用java以及多种设计模式和多线程开发的高性能服务，长期稳定运行，免费使用!',
